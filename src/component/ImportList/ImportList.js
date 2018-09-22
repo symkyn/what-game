9f2f4d79ca48458806d3df4090c52d445f2ff474
symkyn <everett.blake@gamil>
2018-09-22 09:00:55 -0600

    took out the title from the import list

--- src/component/ImportList/ImportList.js
+++ src/component/ImportList/ImportList.js
@@ -3,7 +3,7 @@ import React from 'react';
 const ImportList = (props) => {
     return(
         <div className='import-game-list-item'>
-            <h3>{props.game.name}</h3>
+            {/* <h3>{props.game.name}</h3> */}
             <img src={props.game.thumbnail} alt={`${props.game.name}-thumbnail`} onClick={(e) => props.gameImport(e, props.game.id, props.game.plays)} />
         </div>
     )
