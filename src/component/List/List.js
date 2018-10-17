90db86beff73345accf812bb8d318a6030e0b873
symkyn <everett.blake@gamil>
2018-10-17 16:50:44 -0600

    small styling changes

--- src/component/List/List.js
+++ src/component/List/List.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 import axios from 'axios';
 import { Link } from 'react-router-dom';
 
-
 import  Game  from '../Game/Game';
 import Button from '../Button/Button';
 import './List.css';
