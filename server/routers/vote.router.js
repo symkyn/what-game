f45028912b2a5aa578f8e57774545be60f5f63be
symkyn <everett.blake@gamil>
2018-12-04 16:59:46 -0700

    using session info to vote and not bgg id

--- server/routers/vote.router.js
+++ server/routers/vote.router.js
@@ -5,45 +5,39 @@ const VoteRouter = express.Router();
 VoteRouter.post('/addVote/:gameID', (req, res, next) => {
     const { gameID } = req.params;
     const vote = req.body.vote;
-    const bggid = req.body.username;
-    let userid = null;
-    req.db.userinfo(bggid)
+    let userid = req.session.user.id;
+    const newVote = {gamesid: gameID, userid: userid, vote: vote}
+    req.db.hasvoted(gameID, userid)
         .then(result => {
-            userid = result[0].id
-            const newVote = {gamesid: gameID, userid: userid, vote: vote}
-            req.db.hasvoted(gameID, userid)
-                .then(result => {
-                    if(result.length == 0) {
-                        req.db.Votes.insert(newVote)
-                            .then(() => {
-                                req.db.findaveragevote(gameID)
-                                    .then(result => {
-                                        req.db.updatevoteongame(result[0].Average_Vote, gameID)
-                                            .then(() => res.status(200).send('vote updated successfully'))
-                                            .catch(err => console.warn(err))
-                                    })
-                                    .catch(err => console.warn(err))
-                                res.status(200).send('vote added successfully')
-                            })
-                            .catch(err => console.warn(err))
-                    } else {
-                        req.db.updatevote(vote, gameID, userid)
-                            .then(() => {
-                                req.db.findaveragevote(gameID)
-                                    .then(result => {
-                                        req.db.updatevoteongame(result[0].Average_Vote, gameID)
-                                            .then(() => res.status(200).send('vote updated successfully'))
-                                            .catch(err => console.warn(err))
-                                    })
+            if(result.length == 0) {
+                req.db.Votes.insert(newVote)
+                    .then(() => {
+                        req.db.findaveragevote(gameID)
+                            .then(result => {
+                                req.db.updatevoteongame(result[0].Average_Vote, gameID)
+                                    .then(() => res.status(200).send('vote updated successfully'))
                                     .catch(err => console.warn(err))
                             })
                             .catch(err => console.warn(err))
-                    }
-                }) 
-                .catch(err => console.warn(err))
-                })
+                        res.status(200).send('vote added successfully')
+                    })
+                    .catch(err => console.warn(err))
+            } else {
+                req.db.updatevote(vote, gameID, userid)
+                    .then(() => {
+                        req.db.findaveragevote(gameID)
+                            .then(result => {
+                                req.db.updatevoteongame(result[0].Average_Vote, gameID)
+                                    .then(() => res.status(200).send('vote updated successfully'))
+                                    .catch(err => console.warn(err))
+                            })
+                            .catch(err => console.warn(err))
+                    })
+                    .catch(err => console.warn(err))
+            }
+        }) 
         .catch(err => console.warn(err))
-})
+        })
 
 VoteRouter.get('/getVotes/:gameID', (req, res, next) => {
     const { gameID} = req.params;
